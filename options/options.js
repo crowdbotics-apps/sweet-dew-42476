02ef9a6976e8086018275fc29f0f282f7b10c723
Crowdbotics <team@crowdbotics.com>
2023-07-28 02:43:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sweet-dew-42476",
+  url: "https://sweet-dew-42476.botics.co",
+  api: "https://sweet-dew-42476.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
